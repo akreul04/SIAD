2ee7cdc71a894efdc26fdeb9f34082f46b651a6d
Seed <seed@ubuntu.(none)>
2017-06-21 10:51:51 -0700

    guest functionality 621

--- code/project/newcomment.php
+++ code/project/newcomment.php
@@ -13,33 +13,59 @@
 	}
 
 	function addcomment($postid, $comment){
-		$username = $_SESSION["username"];
-		$sql = "SELECT userid FROM users WHERE username=?";
-		global $mysqli;
-		$stmt = $mysqli->stmt_init();
-		if(!$stmt->prepare($sql))   echo "Prepare failed";
-		$stmt->bind_param('s', $username);
-		if(!$stmt->execute()) echo "Execute failed ";
-		$stmt->bind_result($userid);
-		while ($stmt->fetch()){
+		if(isset($_SESSION["username"])){
+			$username = $_SESSION["username"];
+			$sql = "SELECT userid FROM users WHERE username=?";
+			global $mysqli;
+			$stmt = $mysqli->stmt_init();
+			if(!$stmt->prepare($sql))   echo "Prepare failed";
+			$stmt->bind_param('s', $username);
+			if(!$stmt->execute()) echo "Execute failed ";
+			$stmt->bind_result($userid);
+			while ($stmt->fetch()){
              
-        	}
-		$comment = mysql_real_escape_string($comment);
-		$postid = mysql_real_escape_string($postid);
-		$sql2 = "INSERT INTO comments VALUES (0, ?, ?, ?, NOW());";
-		$stmt2 = $mysqli->stmt_init();
-		if(!$stmt2->prepare($sql2))   echo "Prepare failed";
-		$stmt2->bind_param("iis", $postid, $userid, $comment);
-		if(!$stmt2->execute()) echo "Execute failed ";
+        		}
+			$comment = mysql_real_escape_string($comment);
+			$postid = mysql_real_escape_string($postid);
+			$sql2 = "INSERT INTO comments VALUES (0, ?, ?, ?, NOW());";
+			$stmt2 = $mysqli->stmt_init();
+			if(!$stmt2->prepare($sql2))   echo "Prepare failed";
+			$stmt2->bind_param("iis", $postid, $userid, $comment);
+			if(!$stmt2->execute()) echo "Execute failed ";
 
 
-		if($stmt2 == TRUE){
-			echo "New comment '$comment' has been added";
-		}else
-		{
-			echo "Failed to add the comment '$comment' Error: " .$mysqli->error;
+			if($stmt2 == TRUE){
+				echo "New comment '$comment' has been added";
+			}else{
+				echo "Failed to add the comment '$comment' Error: " .$mysqli->error;
+			}
+		}else{
+			$username = "guest";
+			$sql = "SELECT userid FROM users WHERE username=?";
+			global $mysqli;
+			$stmt = $mysqli->stmt_init();
+			if(!$stmt->prepare($sql))   echo "Prepare failed";
+			$stmt->bind_param('s', $username);
+			if(!$stmt->execute()) echo "Execute failed ";
+			$stmt->bind_result($userid);
+			while ($stmt->fetch()){
+
+			}
+
+			$comment = mysql_real_escape_string($comment);
+			$postid = mysql_real_escape_string($postid);
+			$sql2 = "INSERT INTO comments VALUES (0, ?, ?, ?, NOW());";
+			$stmt2 = $mysqli->stmt_init();
+			if(!$stmt2->prepare($sql2))   echo "Prepare failed";
+			$stmt2->bind_param("iis", $postid, $userid, $comment);
+			if(!$stmt2->execute()) echo "Execute failed ";
+
+			if($stmt2 == TRUE){
+				echo "New comment '$comment' has been added";
+			}else{
+				echo "Failed to add the comment '$comment' Error: " .$mysqli->error;
+			}
 		}
-
 		
 	}
 
